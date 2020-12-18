08d167be8b393961e58228e14cb6672265c89a1a
Manuel <manuelejdelatorre@gmail.com>
2020-12-18 15:58:22 -0300

    Initial commit

---
+++ pages/newaccount.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout'
+
+const Newaccount = () => {
+
+    return (
+        <Layout>
+            <h1>New Account</h1>
+        </Layout>
+
+    )
+}
+
+export default Newaccount;
---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+export default function Products() {
+    return (
+        <div>
+            <Layout>
+                <h1 className="text-2xl text-gray-800 font-light"> Orders </h1>
+            </Layout>
+        </div>
+    )
+}
