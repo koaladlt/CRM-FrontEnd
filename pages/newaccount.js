31cfe09c93cfcf243e4ff21fff5b574b0651160d
Manuel <manuelejdelatorre@gmail.com>
2021-01-15 23:33:09 -0300

    Formik added to newaccount component

--- pages/newaccount.js
+++ pages/newaccount.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Layout from '../components/Layout'
+import { useFormik } from 'formik'
+import * as Yup from "Yup";
 
 const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            lastName: "",
+            email: "",
+            password: ""
+        },
+        onSubmit: value => console.log(value)
+    });
 
     return (
         <Layout>
@@ -9,7 +21,9 @@ const Login = () => {
 
             <div className="flex justify-center mt-5">
                 <div className="w-full max-w-sm">
-                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                        onSubmit={formik.handleSubmit}
+                    >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                                 Name
@@ -17,7 +31,10 @@ const Login = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="name"
                                 type="text"
-                                placeholder="Name">
+                                placeholder="Name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                            >
 
                             </input>
 
@@ -30,7 +47,10 @@ const Login = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="lastName"
                                 type="text"
-                                placeholder="lastName">
+                                placeholder="lastName"
+                                value={formik.values.lastName}
+                                onChange={formik.handleChange}
+                            >
 
                             </input>
 
@@ -43,7 +63,10 @@ const Login = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="email"
                                 type="email"
-                                placeholder="Email">
+                                placeholder="Email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                            >
 
                             </input>
 
@@ -56,7 +79,10 @@ const Login = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="password"
                                 type="password"
-                                placeholder="Password">
+                                placeholder="Password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                            >
 
                             </input>
 
