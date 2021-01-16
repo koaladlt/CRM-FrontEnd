e987d37a9b1240e5079f990f8bf9e0ebf07a3137
Manuel <manuelejdelatorre@gmail.com>
2021-01-15 23:53:28 -0300

    Yup added to newaccount page

--- pages/newaccount.js
+++ pages/newaccount.js
@@ -12,6 +12,17 @@ const Login = () => {
             email: "",
             password: ""
         },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('Name is mandatory'),
+            lastName: Yup.string()
+                .required('Last Name is mandatory'),
+            email: Yup.string()
+                .email('Email is not valid').required('Email is mandatory'),
+            password: Yup.string()
+                .required('Password is mandatory').min(6, 'Password must have at least 6 characters')
+
+        }),
         onSubmit: value => console.log(value)
     });
 
@@ -34,11 +45,18 @@ const Login = () => {
                                 placeholder="Name"
                                 value={formik.values.name}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             >
 
                             </input>
 
                         </div>
+
+                        {formik.touched.name && formik.errors.name ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.name}</p>
+                            </div>
+                        ) : null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
@@ -56,6 +74,12 @@ const Login = () => {
 
                         </div>
 
+                        {formik.touched.lastName && formik.errors.lastName ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.lastName}</p>
+                            </div>
+                        ) : null}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                                 Email
@@ -72,6 +96,12 @@ const Login = () => {
 
                         </div>
 
+                        {formik.touched.email && formik.errors.email ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.email}</p>
+                            </div>
+                        ) : null}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                 Password
@@ -87,6 +117,12 @@ const Login = () => {
                             </input>
 
                         </div>
+
+                        {formik.touched.password && formik.errors.password ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.password}</p>
+                            </div>
+                        ) : null}
 
                         <input
                             type="submit"
