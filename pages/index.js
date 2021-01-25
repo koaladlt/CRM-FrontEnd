a0a31c664773e82611740994d2321281284264b5
Manuel <manuelejdelatorre@gmail.com>
2021-01-25 19:19:20 -0300

    Showing clients information

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,51 @@
 import Layout from '../components/Layout'
+import { gql, useQuery } from '@apollo/client'
 
-export default function Index() {
+const GET_CLIENTS = gql`
+query getClientsBySeller {
+  getClientsBySeller {
+    name
+    lastName
+    company
+    email
+  }
+}
+`;
+
+const Index = () => {
+
+  const { data, loading, error } = useQuery(GET_CLIENTS)
+
+  console.log(data)
+  console.log(loading)
+  console.log(error)
+
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light"> Clients </h1>
+
+        <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2 ">Name</th>
+              <th className="w-1/5 py-2 ">Company</th>
+              <th className="w-1/5 py-2 ">Email</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {data.getClientsBySeller.map((client) => (
+              <tr key={client.id}>
+                <td className="border px-4  py-2 ">{client.name} {client.lastName}</td>
+                <td className="border px-4  py-2 ">{client.company}</td>
+                <td className="border px-4  py-2 ">{client.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Layout>
     </div>
   )
 }
 
+export default Index;
