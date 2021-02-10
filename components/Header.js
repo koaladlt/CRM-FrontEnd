ba82114b8a8fffa117ed43849c08e889547719d8
Manuel <manuelejdelatorre@gmail.com>
2021-02-10 15:57:04 -0300

    logout working properly

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
 
 const GET_USER = gql`
 query getUser {
@@ -17,16 +18,28 @@ query getUser {
 const Header = () => {
 
     const { data, loading, error } = useQuery(GET_USER)
+    const router = useRouter()
 
     if (loading) return null
     console.log(data)
 
     const { name } = data.getUser
-    return (
-        <div>
-            <p>Hola: {name}</p>
 
-            <button type="button">Log Out</button>
+    const logOut = () => {
+        localStorage.removeItem('token')
+        router.push('/login')
+
+    }
+    return (
+        <div className="flex justify-between mb-6">
+            <p className="mr-2">Hola: {name}</p>
+
+            <button
+                onClick={() => logOut()}
+                type="button"
+                className="bg-blue-800 w-full sm:w-auto font bold uppercase text-xs rounded py-1 px-2 text-white shadow-md">
+                Log Out
+            </button>
         </div>
     )
 }
