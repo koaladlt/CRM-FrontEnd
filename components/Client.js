cd50dc7caf6e3ae69ceeab5843a66b1c5f8c82c3
Manuel <manuelejdelatorre@gmail.com>
2021-02-12 12:33:44 -0300

    Formik form added

--- components/Client.js
+++ components/Client.js
@@ -99,7 +99,7 @@ const Client = ({ client }) => {
                     onClick={() => editClient()}
                     className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold">
                     Edit
-                    <svg className="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                 </button>
             </td>
         </tr>
--- pages/editclient/[pid].js
+++ pages/editclient/[pid].js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
 
 const EditClient = () => {
     const router = useRouter();
@@ -7,7 +8,137 @@ const EditClient = () => {
     console.log(id)
 
     return (
-        <h1>Edit Client</h1>
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Edit Client</h1>
+
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-lg">
+
+                    <form
+                        className="bg-white shadow-md px-8 pt-6 pb-8 mb-4 "
+                    // onSubmit={formik.handleSubmit}
+                    >
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                                Name
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="name"
+                                type="text"
+                                placeholder="Client's name"
+                            // onChange={formik.handleChange}
+                            // onBlur={formik.handleBlur}
+                            // value={formik.values.name}
+                            >
+
+                            </input>
+
+                        </div>
+                        {/* 
+                        {formik.touched.name && formik.errors.name ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.name}</p>
+                            </div>
+                        ) : null} */}
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+                                Last Name
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="lastName"
+                                type="text"
+                                placeholder="Client's Last Name"
+                            // onChange={formik.handleChange}
+                            // onBlur={formik.handleBlur}
+                            // value={formik.values.lastName}
+                            >
+
+                            </input>
+
+                        </div>
+
+                        {/* {formik.touched.lastName && formik.errors.lastName ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.lastName}</p>
+                            </div>
+                        ) : null} */}
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
+                                Company
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="company"
+                                type="text"
+                                placeholder="Company's name"
+                            // onChange={formik.handleChange}
+                            // onBlur={formik.handleBlur}
+                            // value={formik.values.company}
+                            >
+
+                            </input>
+
+                        </div>
+
+                        {/* {formik.touched.company && formik.errors.company ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.company}</p>
+                            </div>
+                        ) : null} */}
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="email"
+                                type="text"
+                                placeholder="Email address"
+                            // onChange={formik.handleChange}
+                            // onBlur={formik.handleBlur}
+                            // value={formik.values.email}
+                            >
+
+                            </input>
+
+                        </div>
+
+                        {/* {formik.touched.email && formik.errors.email ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.email}</p>
+                            </div>
+                        ) : null} */}
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+                                Phone number
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="phone"
+                                type="tel"
+                                placeholder="Phone number"
+                            // onChange={formik.handleChange}
+                            // onBlur={formik.handleBlur}
+                            // value={formik.values.phone}
+                            >
+
+                            </input>
+
+                        </div>
+
+                        <input
+                            type="submit"
+                            className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-grey-900"
+                            value="Save client" />
+
+
+                    </form>
+
+                </div>
+            </div>
+        </Layout>
     )
 }
 
