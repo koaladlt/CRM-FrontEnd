5d558b52f6afe04b72a2b3bc0b3f05653e002bf3
Manuel <manuelejdelatorre@gmail.com>
2021-02-15 18:18:15 -0300

    Yup and formik added to new product page

--- pages/newproduct.js
+++ pages/newproduct.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import Layout from '../components/Layout'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 
 const NewProduct = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            stock: '',
+            price: '',
+
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().required("Product's name is mandatory"),
+            stock: Yup.number()
+                .required("Product's stock is mandatory")
+                .positive('You can add negative numbers')
+                .integer('Stock must be an integer number'),
+            price: Yup.number()
+                .required("Product's price is mandatory")
+                .positive("You can't add a negative number")
+        })
+    })
+
     return (
         <Layout>
             <h1 className="text-2xl text-gray-800 font-light">Create New Product</h1>
@@ -11,7 +34,7 @@ const NewProduct = () => {
 
                     <form
                         className="bg-white shadow-md px-8 pt-6 pb-8 mb-4 "
-                    // onSubmit={formik.handleSubmit}
+                        onSubmit={formik.handleSubmit}
                     >
 
                         <div className="mb-4">
@@ -22,14 +45,20 @@ const NewProduct = () => {
                                 id="name"
                                 type="text"
                                 placeholder="Product's name"
-                            // onChange={formik.handleChange}
-                            // onBlur={formik.handleBlur}
-                            // value={formik.values.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.name}
                             >
 
                             </input>
 
                         </div>
+
+                        {formik.touched.name && formik.errors.name ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.name}</p>
+                            </div>
+                        ) : null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="stock">
@@ -39,14 +68,20 @@ const NewProduct = () => {
                                 id="stock"
                                 type="number"
                                 placeholder="Product's stock"
-                            // onChange={formik.handleChange}
-                            // onBlur={formik.handleBlur}
-                            // value={formik.values.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.stock}
                             >
 
                             </input>
 
                         </div>
+
+                        {formik.touched.stock && formik.errors.stock ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.stock}</p>
+                            </div>
+                        ) : null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">
@@ -56,14 +91,20 @@ const NewProduct = () => {
                                 id="price"
                                 type="number"
                                 placeholder="Product's price"
-                            // onChange={formik.handleChange}
-                            // onBlur={formik.handleBlur}
-                            // value={formik.values.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.price}
                             >
 
                             </input>
 
                         </div>
+
+                        {formik.touched.price && formik.errors.price ? (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">{formik.errors.price}</p>
+                            </div>
+                        ) : null}
 
                         <input
                             type="submit"
