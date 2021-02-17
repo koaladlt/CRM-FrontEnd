907c45749b5058dcb16b687a0727ac57ca3c3008
Manuel <manuelejdelatorre@gmail.com>
2021-02-17 15:45:25 -0300

    Adding react-select

--- pages/neworder.js
+++ pages/neworder.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import NewProduct from './newproduct';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout'
+import Select from 'react-select'
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+]
 
 const NewOrder = () => {
     return (
         <Layout>
             <h1 className="text-2xl text-gray-800 font-light">New Order</h1>
+
+            <Select
+                options={options}
+                isMulti={true}
+            />
         </Layout>
+
+
     )
 }
 
