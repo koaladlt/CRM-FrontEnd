4c35f99ce497631ff77966ec70959777ef335498
Manuel <manuelejdelatorre@gmail.com>
2021-02-17 15:29:22 -0300

    Creating page for new order

---
+++ pages/neworder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewProduct from './newproduct';
+import Layout from '../components/Layout'
+
+const NewOrder = () => {
+    return (
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">New Order</h1>
+        </Layout>
+    )
+}
+
+export default NewOrder;
