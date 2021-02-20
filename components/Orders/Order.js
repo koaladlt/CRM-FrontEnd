1c332731f4e6fee0709cd600fcbe2c7f39380f68
Manuel <manuelejdelatorre@gmail.com>
2021-02-20 13:16:19 -0300

    Showing orders in page

---
+++ components/Orders/Order.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Order = ({ order }) => {
+    return (
+        <h1>{order.total}</h1>
+    )
+}
+
+export default Order;
--- pages/orders.js
+++ pages/orders.js
@@ -1,7 +1,36 @@
 import Layout from '../components/Layout'
 import Link from 'next/link'
+import { gql, useQuery } from '@apollo/client';
+import Order from '../components/Orders/Order'
 
-export default function Orders() {
+const GET_ORDERS_BY_SELLER = gql`
+query getOrdersBySeller {
+    getOrdersBySeller {
+        id
+        order {
+            id
+            amount
+        }
+        client
+        seller
+        total
+        state
+    }
+}
+
+`;
+
+
+
+const Orders = () => {
+
+    const { data, loading, error } = useQuery(GET_ORDERS_BY_SELLER);
+
+    if (loading) return null;
+    console.log(data);
+
+    const { getOrdersBySeller } = data;
+
     return (
         <div>
             <Layout>
@@ -10,7 +39,19 @@ export default function Orders() {
                 <Link href="/neworder">
                     <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">New Order </a>
                 </Link>
+
+                {getOrdersBySeller.length === 0 ? (
+                    <p className="mt-5 text-center text-2xl">There are no orders yet</p>
+                ) : (
+                        getOrdersBySeller.map((order) => (
+                            <Order
+                                key={order.id}
+                                order={order} />
+                        ))
+                    )}
             </Layout>
         </div>
     )
 }
+
+export default Orders;
