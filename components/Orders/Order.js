72e4ee8696209655689fe9e3a383a0643faca433
Manuel <manuelejdelatorre@gmail.com>
2021-02-21 17:38:05 -0300

    Showing phone and email info

--- components/Orders/Order.js
+++ components/Orders/Order.js
@@ -20,6 +20,20 @@ const Order = ({ order }) => {
                     Client: {name} {lastName}
                 </p>
 
+                {email && (
+                    <p className="flex items-center my-2">
+                        <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path></svg>
+                        {email}
+                    </p>
+                )}
+
+                {phone && (
+                    <p className="flex items-center my-2">
+                        <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"></path></svg>
+                        {phone}
+                    </p>
+                )}
+
                 <h2 className="text-gray-800 font-bold mt-10"> Order State: </h2>
 
                 <select className="mt-2 appearance-none bg-blue-600 border border-blue-600 text-white p-2 text-center rounded leading-tight focus:outline-none focus:bg-blue-600 focus:border-blue-500 uppercase text-xs font-bold"
